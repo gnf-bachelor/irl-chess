c497c39f1113f988ecb46e85e87db9a11bf3f0d5
Fabian Scott <76645786+FabianScott@users.noreply.github.com>
2024-02-06 18:19:03 +0100

    Update sunfish_permuted.py

--- project/sunfish_permuted.py
+++ project/sunfish_permuted.py
@@ -46,8 +46,8 @@ def run_sun(df,
 
 
 if __name__ == '__main__':
-    if os.getcwd().split('\\')[-1] != 'irl-chess':
-        os.chdir('../')
+    # if os.getcwd().split('\\')[-1] != 'irl-chess':
+    #     os.chdir('../')
     from project import policy_walk, get_midgame_boards, get_best_move, piece, download_lichess_pgn
 
     n_files = 2
