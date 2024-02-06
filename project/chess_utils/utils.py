5b0d9270be45eda97d2ab304f7fda7b0549a8d26
Gabriel Agerholm Ruge <gabrielruge@gmail.com>
2024-02-06 16:05:04 +0100

    Added policy_walk_depth and policy_walk to utils

--- project/chess_utils/utils.py
+++ project/chess_utils/utils.py
@@ -210,13 +210,27 @@ def depth_first_search(starting_board: chess.Board,
 
     return boards_not_seen, boards_seen
 
-
-def prob_dist(R, energy, alpha, prior=lambda R: 1):
-    prob = np.exp(alpha * energy) * prior(R)
-    return prob
-
+def log_prob_dist(R, energy, alpha, prior=lambda R: 1):
+    log_prob = alpha * energy + np.log(prior(R))
+    return log_prob
 
 def policy_walk(R, states, moves, delta=1e-3, epochs=10, depth=3, alpha=2e-2):
+    """ Policy walk algorithm over given class of reward functions.
+    Iterates over the initial reward function by perterbing each dimension uniformly and then
+    accepting the new reward function with probability proportional to how much better they explain the given trajectories. 
+
+    Args:
+        R (_type_): The reward function (heuristic for statically evaluation a board).
+        states (_type_): _description_
+        moves (_type_): _description_
+        delta (_type_, optional): _description_. Defaults to 1e-3.
+        epochs (int, optional): _description_. Defaults to 10.
+        depth (int, optional): _description_. Defaults to 3.
+        alpha (_type_, optional): _description_. Defaults to 2e-2.
+
+    Returns:
+        _type_: _description_
+    """
     for epoch in tqdm(range(epochs)):
         add = np.random.rand(R.shape[0]).astype(R.dtype) * (delta / 2)
         R_ = R + add
@@ -224,22 +238,81 @@ def policy_walk(R, states, moves, delta=1e-3, epochs=10, depth=3, alpha=2e-2):
         Q_policy = np.zeros(len(states))
         i = 0
         energy_new, energy_old = 0, 0
-        for state, move in tqdm(zip(states, moves), total=len(states)):
+        for state, move in tqdm(zip(states, moves), total=len(states), desc = 'Policy walking over reward functions'):
             state.push_san(move)
-            _, Q_old = get_best_move(board=state, R=R, depth=depth - 1, white=state.turn)
-            _, Q_new = get_best_move(board=state, R=R_, depth=depth - 1, white=state.turn)
+            _, Q_old = get_best_move(board=state, R=R, depth=depth-1)
+            _, Q_new = get_best_move(board=state, R=R_, depth=depth-1)
             state.pop()
             # _, Q_old_energy = get_best_move(board=state, R=R, depth=depth)
-
+            
             Q_moves[i] = Q_old
             Q_policy[i] = Q_new
-
+            
             energy_old += Q_old
             energy_new += Q_new
 
             i += 1
-            prob = min(1, prob_dist(R_, energy_new, alpha=alpha) / prob_dist(R_, energy_old, alpha=alpha))
+
+            log_prob = min(0, log_prob_dist(R_, energy_new, alpha=alpha) - log_prob_dist(R, energy_old, alpha=alpha))
+
             if np.sum(Q_policy < Q_moves):
-                if np.random.rand(1).item() < prob:
+                if log_prob > -1e7 and np.random.rand(1).item() < np.exp(log_prob):
                     R = R_
     return R
+
+
+def policy_walk_depth(R, boards, moves, delta=1e-3, epochs=10, depth_max=3, alpha=2e-2, time_max=np.inf, timer_moves=False):
+    """ Policy walk algorithm over the depth of the search.
+    Begins with a uniform distribution over search depths and iterates by perterbing each dimension uniformly and then
+    accepting the new softmax distribution of search depths with probability proportional to how much better they explain the given trajectories. 
+
+    Args:
+        R (_type_): The reward function (heuristic for statically evaluation a board).
+        boards (_type_): _description_
+        moves (_type_): _description_
+        delta (_type_, optional): _description_. Defaults to 1e-3.
+        epochs (int, optional): _description_. Defaults to 10.
+        depth_max (int, optional): _description_. Defaults to 3.
+        alpha (_type_, optional): _description_. Defaults to 2e-2.
+        time_max (_type_, optional): _description_. Defaults to np.inf.
+        timer_moves (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        ndarray : final array of search depth probability distribution pre-softmax. Indexes signify depth starting from 1. 
+    """
+    depth_dist = np.ones(depth_max)
+    start = time()
+    for epoch in tqdm(range(epochs)):
+        add = np.random.uniform(low=-delta, high=delta, size=depth_dist.shape[0]).astype(depth_dist.dtype) # * (delta / 2)
+        depth_dist_ = depth_dist + add
+        Q_moves = np.zeros(len(boards))
+        Q_policy = np.zeros(len(boards))
+        i = 0
+        energy_new, energy_old = 0, 0
+        for board, move in tqdm(zip(boards, moves), total=len(boards)):
+            board.push_san(move)
+            depth1 = softmax_choice(depth_dist)
+            depth2 = softmax_choice(depth_dist_)
+            _, Q_old = get_best_move(board=board, R=R, depth=depth1, timer=timer_moves, white=board.turn)
+            _, Q_new = get_best_move(board=board, R=R, depth=depth2, timer=timer_moves, white=board.turn)
+            if Q_new is None or Q_old is None:
+                continue
+            board.pop()
+            
+            Q_moves[i] = Q_old
+            Q_policy[i] = Q_new
+            
+            energy_old += Q_old
+            energy_new += Q_new
+
+            i += 1
+            # prob = min(1, prob_dist(depth_dist_, energy_new, alpha=alpha)/prob_dist(depth_dist, energy_old, alpha=alpha))
+
+            log_prob = min(0, log_prob_dist(depth_dist_, energy_new, alpha=alpha) - log_prob_dist(depth_dist, energy_old, alpha=alpha))
+            
+            if np.sum(Q_policy < Q_moves):
+                if log_prob > -1e7 and np.random.rand(1).item() < np.exp(log_prob): 
+                    depth_dist = copy(depth_dist_)
+            if start - time() > time_max:
+                return depth_dist
+    return depth_dist
