d3d9d61b9b54b2d486baf8e00b8773a499dac103
NikolajT84 <97594388+NikolajT84@users.noreply.github.com>
2024-03-09 14:09:01 +0100

    Update submit-native.sh

--- hpc-submit/submit-native.sh
+++ hpc-submit/submit-native.sh
@@ -3,7 +3,7 @@
 #BSUB -e sunfish-native%J.err
 #BSUB -q hpc
 #BSUB -n 4
-#BSUB -R "rusage[mem=16G]"
+#BSUB -R "rusage[mem=32G]"
 #BSUB -R "span[hosts=1]"
 #BSUB -W 24:00
 ### -- send notification at start --
