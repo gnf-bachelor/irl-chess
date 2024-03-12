ed1db2ecb089a7c13a8e1f708f976f43cc01a251
Fabian Scott <76645786+FabianScott@users.noreply.github.com>
2024-03-12 11:45:03 +0100

    Update get_boards to one function

--- irl_chess/sunfish_native_pw.py
+++ irl_chess/sunfish_native_pw.py
@@ -92,12 +92,56 @@ def plot_R(Rs, path=None):
 #     move = sunfish_move_mod(state, pst_new,time_limit,True)
 #     actions.append(move)
 
+
+def is_valid_game(game, config_data):
+    # Add time control check
+    try:
+        elo_check_white = config_data['min_elo'] < int(game.headers['WhiteElo']) < config_data['max_elo']
+        elo_check_black = config_data['min_elo'] < int(game.headers['BlackElo']) < config_data['max_elo']
+        length_check = len([el for el in game.mainline_moves()]) < config_data['n_endgame']
+    except KeyError:
+        return False
+    except ValueError:
+        return False
+    return elo_check_white and elo_check_black and length_check
+
+
+def get_states(websites_filepath, file_path_data, config_data):
+    pgn_paths = download_lichess_pgn(websites_filepath=websites_filepath,
+                                     file_path_data=file_path_data,
+                                     overwrite=config_data['overwrite'],
+                                     n_files=config_data['n_files'])
+
+    chess_boards = []
+    i = 0
+    while len(chess_boards) < config_data['n_boards']:
+        pgn_path = pgn_paths[i]
+        progress = 0
+        with open(pgn_path) as pgn:
+            size = os.path.getsize(pgn_path)
+            with tqdm(total=size, desc=f'Looking through file {i}') as pbar:
+                while len(chess_boards) < config_data['n_boards']:
+                    game = chess.pgn.read_game(pgn)
+                    if is_valid_game(game, config_data=config_data):
+                        chess_boards.append(get_board_after_n(game, config_data['n_midgame']))
+                        chess_boards.append(get_board_after_n(game, config_data['n_endgame']))
+                    pbar.update(pgn.tell() - progress)
+                    progress = pgn.tell()
+                    if size <= progress:
+                        break
+            i += 1
+
+    sunfish_boards = [board2sunfish(board, eval_pos(board)) for board in chess_boards]
+    return sunfish_boards
+
+
 if __name__ == '__main__':
     print(os.getcwd())
     if os.getcwd()[-len('irl-chess'):] != 'irl-chess':
         os.chdir('../')
         print(os.getcwd())
-    from irl_chess import get_midgame_boards, piece, load_lichess_dfs, create_sunfish_path, plot_permuted_sunfish_weights
+    from irl_chess import piece, create_sunfish_path, \
+        plot_permuted_sunfish_weights, download_lichess_pgn
 
     path_config = join(os.getcwd(), 'experiment_configs', 'sunfish_permutation_native', 'config.json')
     with open(path_config, 'r') as file:
@@ -107,30 +151,15 @@ if __name__ == '__main__':
         os.makedirs(out_path, exist_ok=True)
         copy2(path_config, join(out_path, 'config.json'))
 
-    n_files = config_data['n_files']
-    overwrite = config_data['overwrite']
-    version = config_data['version']
     websites_filepath = join(os.getcwd(), 'downloads', 'lichess_websites.txt')
     file_path_data = join(os.getcwd(), 'data', 'raw')
 
-    df = load_lichess_dfs(websites_filepath=websites_filepath,
-                          file_path_data=file_path_data,
-                          n_files=n_files,
-                          overwrite=overwrite)
-
-    boards, _ = get_midgame_boards(df,
-                                   n_boards=config_data['n_boards'],
-                                   min_elo=config_data['min_elo'],
-                                   max_elo=config_data['max_elo'],
-                                   sunfish=False,
-                                   n_steps=15)
-
-    # states_boards = [get_board_after_n(game, 15) for game in games[:n_games]]
-    states = [board2sunfish(board, eval_pos(board)) for board in boards]
+    sunfish_boards = get_states(websites_filepath=websites_filepath,
+                                file_path_data=file_path_data,
+                                config_data=config_data)
 
     epochs = config_data['epochs']
     time_limit = config_data['time_limit']
-    delta = config_data['delta']
     save_every = config_data['save_every']
     permute_all = config_data['permute_all']
     permute_idxs = char_to_idxs(config_data['permute_char'])
@@ -148,11 +177,12 @@ if __name__ == '__main__':
     R = np.array([val for val in piece.values()]).astype(float)
     R_new = copy.copy(R)
     R_new[permute_idxs] = R_noisy_vals
+    delta = config_data['delta']
 
-    with Parallel(n_jobs=n_threads) as parallel:
-        actions_true = parallel(delayed(sunfish_move_mod)(state, pst, time_limit, True)
-                                for state in tqdm(states, desc='Getting true moves',))
-        for epoch in tqdm(range(epochs), desc='Epoch'):
+    with Parallel(n_jobs=config_data['n_threads']) as parallel:
+        actions_true = parallel(delayed(sunfish_move_mod)(state, pst, config_data['time_limit'], True)
+                                for state in tqdm(sunfish_boards, desc='Getting true moves', ))
+        for epoch in tqdm(range(config_data['epochs']), desc='Epoch'):
             if permute_all:
                 add = np.random.uniform(low=-delta, high=delta, size=len(permute_idxs)).astype(R.dtype)
                 R_new[permute_idxs] += add
@@ -161,25 +191,27 @@ if __name__ == '__main__':
                 R_new[choice] += np.random.uniform(low=-delta, high=delta, size=1).item()
 
             pst_new = get_new_pst(R_new)
-            states = [board2sunfish(board, eval_pos(board, R_new)) for board in boards]
-            actions_new = parallel(delayed(sunfish_move_mod)(state, pst_new, time_limit, True)
-                                   for state in tqdm(states, desc='Getting new actions'))
+            actions_new = parallel(delayed(sunfish_move_mod)(state, pst_new, config_data['time_limit'], True)
+                                   for state in tqdm(sunfish_boards, desc='Getting new actions'))
 
-            acc = sum([a == a_new for a, a_new in list(zip(actions_true, actions_new))]) / n_boards
-            change_weights = np.random.rand() < np.exp(acc)/(np.exp(acc) + np.exp(last_acc)) if config_data['version'] == 'v1_native_multi' else acc >= last_acc
+            acc = sum([a == a_new for a, a_new in list(zip(actions_true, actions_new))]) / config_data['n_boards']
+            change_weights = np.random.rand() < np.exp(acc) / (np.exp(acc) + np.exp(last_acc)) if config_data[
+                                                                                                      'version'] == 'v1_native_multi' else acc >= last_acc
             if change_weights:
-                print(f'Changed weights!')
                 R = copy.copy(R_new)
                 last_acc = copy.copy(acc)
             accuracies.append((acc, last_acc))
 
-            if epoch % decay_step == 0 and epoch != 0:
+            if epoch % config_data['decay_step'] == 0 and epoch != 0:
                 delta *= decay
 
-            if save_every is not None and save_every and epoch % save_every == 0:
+            if config_data['save_every'] is not None and config_data['save_every'] and epoch % config_data['save_every'] == 0:
                 pd.DataFrame(R.reshape((-1, 1)), columns=['Result']).to_csv(join(out_path, f'{epoch}.csv'),
-                                                                             index=False)
-            if plot_every is not None and plot_every and epoch % plot_every == 0:
-                plot_permuted_sunfish_weights(config_data=config_data, out_path=out_path, epoch=epoch, accuracies=accuracies)
+                                                                            index=False)
+            if config_data['plot_every'] is not None and config_data['plot_every'] and epoch % config_data['plot_every'] == 0:
+                plot_permuted_sunfish_weights(config_data=config_data,
+                                              out_path=out_path,
+                                              epoch=epoch,
+                                              accuracies=accuracies)
 
             print(f'Current accuracy: {acc}, {last_acc}')
