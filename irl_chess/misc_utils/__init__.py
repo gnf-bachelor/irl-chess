160aa654f15a93884b323bcc64956b426faba534
Fabian Scott <76645786+FabianScott@users.noreply.github.com>
2024-03-12 17:06:38 +0100

    More refactoring of paths

--- irl_chess/misc_utils/__init__.py
+++ irl_chess/misc_utils/__init__.py
@@ -1,2 +1,2 @@
-from irl_chess.misc_utils.load_save_utils import fix_cwd, load_config, create_result_path
+from irl_chess.misc_utils.load_save_utils import fix_cwd, load_config, create_result_path, get_states
 from irl_chess.misc_utils.utils import union_dicts
