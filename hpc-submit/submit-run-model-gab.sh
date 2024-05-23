d810de4bf63232a41bc0fb16c38ba18586b72298
Dolphus2 <gabrielruge@gmail.com>
2024-05-23 18:12:30 +0200

    gabriel hpc submit

---
+++ hpc-submit/submit-run-model-gab.sh
@@ -0,0 +1,17 @@
+#BSUB -J sunfish-run
+#BSUB -o /zhome/5c/0/167753/DTU/Spring_2024/Bachelor/irl-chess/hpc-logs/sunfish-run/%J.out
+#BSUB -e /zhome/5c/0/167753/DTU/Spring_2024/Bachelor/irl-chess/hpc-logs/sunfish-run/%J.err
+#BSUB -q hpc
+#BSUB -n 24
+#BSUB -R "rusage[mem=1G]"
+#BSUB -R "span[hosts=1]"
+#BSUB -W 24:00
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+# end of BSUB options
+
+module load python3.10.13
+source bachelor/bin/activate
+python3 -m irl_chess.run_model
