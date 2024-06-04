ef34043288b1c7ed0cf2ea4a391d64529bdf58ae
NikolajT84 <97594388+NikolajT84@users.noreply.github.com>
2024-06-04 09:57:59 +0200

    .

--- irl_chess/run_model.py
+++ irl_chess/run_model.py
@@ -10,6 +10,28 @@ if __name__ == '__main__':
     base_config_data, model_config_data = load_config()
     config_data = union_dicts(base_config_data, model_config_data)
 
+    base_config_data['overwrite'] = False
+    base_config_data['n_files'] = 6
+    base_config_data['time_control'] = False
+    base_config_data['min_elo'] = 1900
+    base_config_data['max_elo'] = 2000
+    base_config_data['n_midgame'] = 15
+    base_config_data['n_endgame'] = 30
+    base_config_data['n_boards'] = 10000
+    base_config_data['epochs'] = 400
+    base_config_data['n_threads'] = 24
+    base_config_data['plot_every'] = 50
+    base_config_data['save_every'] = 50
+
+    base_config_data['plot_char'] = ["P", "N", "B", "R", "Q"]
+    base_config_data['permute_char'] = ["N", "B", "R", "Q"]
+    base_config_data['R_start'] = [100, 100, 100, 100, 100, 60000]
+
+    base_config_data['move_function'] = "sunfish_move"
+    base_config_data['R_true'] = [100, 280, 320, 480, 920, 60000]
+    base_config_data['model'] = "sunfish_GRW"
+    base_config_data['move_percentage_data'] = False
+
     match config_data['model']: # Load the model specified in the "base_config" file. Make sure the "model" field is set 
                                 # correctly and that a model_result_string function is defined to properly store the results.
         case "sunfish_GRW": # Sunfish Greedy Random Walk
