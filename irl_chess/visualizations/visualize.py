425e6631aaca16254010a7102aebce67db9ed958
FabScott <143505755+FabScott@users.noreply.github.com>
2024-06-05 10:56:00 +0200

    New color

--- irl_chess/visualizations/visualize.py
+++ irl_chess/visualizations/visualize.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from os.path import join
 from matplotlib import pyplot as plt
 
-sunfish_palette_name = 'mako'
+sunfish_palette_name = "Spectral"
 maia_palette_name = 'flare'
 
 result_strings = ['Result', 'RpstResult', 'RHResult']
